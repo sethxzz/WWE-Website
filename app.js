afcb3f83d364ca0c029b8611522c3518f1c22f0b
Sethxzz <luccaportella12@gmail.com>
2025-07-26 23:36:41 -0300

    Mensagem do commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const nextButton = document.querySelector('.next-btn');
+const video = document.querySelector('.hero-video');
+
+const movieList = ['videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4'];
+
+let index = 0;
+
+nextButton.addEventListener('click', function() {
+    index += 1;
+    video.src = movieList[index];
+
+    if (index === 3) {
+        index = -1;
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById("meuVideo");
+
+  video.addEventListener("loadedmetadata", function () {
+    video.currentTime = 4; // começa no segundo 5
+  });
+});
